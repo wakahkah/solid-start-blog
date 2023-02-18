a41db76e3587bac713ba3bf51524a10723ac6f63
rebekah lee <kahkah78@gmail.com>
2023-02-18 01:16:08 -0700

    [Fix] for seo

--- src/root.tsx
+++ src/root.tsx
@@ -22,6 +22,7 @@ export default function Root() {
         <Title>SolidStart - With MDX</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta name="description" content="Soild Start Blog all for testing." />
       </Head>
       <Body>
         <ErrorBoundary>
